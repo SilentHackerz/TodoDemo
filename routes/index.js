e92287324466f17b72453557fe5b075ac65ebefe
Ayyub Raza <ayyubrazazain@gmail.com>
2018-03-24 13:35:26 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express=require('express');
 var router = express.Router();
 var db = require('../models');
 //get - /api/todos-for getting the list
-//find-get
+
+//findall-get
 router.get('/', function(req,res){
 //res.send("this is from routes folder index route");
 db.Todo.find()
@@ -24,6 +25,49 @@ res.json(newtodos);
 .catch(function(err){
 res.send(err);
 });
+});
+
+module.exports = router;
+
+//get-findById
+
+router.get('/:todoId',function(req,res){
+db.Todo.findById(req.params.todoId)
+.then(function(todos){
+res.json(todos);
+})
+.catch(function(err){
+res.send(err);
+});
+});
+
+//findoneandupdate-put
+router.put('/:todoId',function(req,res){
+
+    db.Todo.findByIdAndUpdate({_id:req.params.todoId},req.body)
+
+        .then(function(updatetodos){
+        res.json(updatetodos);
+        })
+
+        .catch(function(err){
+        res.send(err);
+        });
+});
+
+//delete
+router.delete('/:todoId',function(req,res){
+
+    db.Todo.remove({_id:req.params.todoId})
+
+        .then(function(){
+        res.json({message : "deleted "})
+
+        })
+
+        .catch(function(err){
+        res.send(err);
+        });
 });
 
 module.exports = router;
